8306b555e87547966b1186f3d7d799aa832c593e
nataries <natalizastavnaya@icloud.com>
2026-01-20 17:25:39 +0300

    Add while loop

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85ba78d136def6c4b8d44a671b99161051bdfe6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff065b7f3801c238bc92316fd443f9f0b3dba71d")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- backend/program.cs
+++ backend/program.cs
@@ -1,4 +1,19 @@
-﻿Console.WriteLine("Введите своё имя: ");
-string? name = Console.ReadLine();
-Console.WriteLine($"Привет, {name}!");
-Console.WriteLine($"День недели: {DateTime.Now.DayOfWeek}");
+﻿using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Text;
+
+while (true)
+{
+    Console.WriteLine("Введите своё имя: ");
+    string? name = Console.ReadLine();
+    Console.WriteLine($"Привет, {name}!");
+    Console.WriteLine($"День недели: {DateTime.Now.DayOfWeek}");
+    Console.WriteLine("Хотите продолжить (y/n)?");
+    string answer = Console.ReadLine();
+    if (answer.ToLower() != "y")
+    {
+        break;
+    }
+}
